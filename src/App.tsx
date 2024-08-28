812136e795eed4ad82fa25b394623b4c3000f24e
vkiragi <varunkiragi@icloud.com>
2024-08-28 15:10:54 -0700

    optimized for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import "./App.css";
 import CarAffordabilityCalculator from "./components/CarAffordabilityCalculator";
--- src/components/CarAffordabilityCalculator.tsx
+++ src/components/CarAffordabilityCalculator.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { cars, Car } from "../carData"; // Make sure this path is correct
+import { cars, Car } from "../carData";
 
 const CarAffordabilityCalculator = () => {
   const { make, model, price } = useParams<{
